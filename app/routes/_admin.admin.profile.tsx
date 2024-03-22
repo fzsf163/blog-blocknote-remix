baa01bf499d1a93bd67e0c6bc25a7370a69848e6
fzsf163 <yofaisol619@gmail.com>
2024-03-23 00:31:13 +0600

    profle data

--- app/routes/_admin.admin.profile.tsx
+++ app/routes/_admin.admin.profile.tsx
@@ -1,16 +1,20 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
 import { authenticator } from "~/utils/auth.server";
 import { requireUserSession } from "~/utils/session.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const session = await requireUserSession(request);
+  let userid = session.data?.sessionKey?.userID;
   // return json({ login: session });
   // await authenticator.isAuthenticated(request, {
   //   failureRedirect: "/admin",
   // });
-  return "ok";
+  return userid;
 };
 
 export default function Admin_Profile() {
-  return <div>Welocome to admin profile</div>;
+  const laoderData = useLoaderData<typeof loader>();
+  return <div>Welocome to admin profile {laoderData}</div>;
 }
