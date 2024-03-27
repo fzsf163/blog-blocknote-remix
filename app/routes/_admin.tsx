a68804232022883e5b79d439352347558a5f4aa7
fzsf163 <yofaisol619@gmail.com>
2024-03-27 23:31:11 +0600

    animated side nav

--- app/routes/_admin.tsx
+++ app/routes/_admin.tsx
@@ -15,7 +15,14 @@ import { cssBundleHref } from "@remix-run/css-bundle";
 
 import Hover_Box from "~/components/hoverCard";
 import Dialuge from "~/components/dialouge";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  LayoutDashboard,
+  SquareGantt,
+  UserCog,
+  LocateFixed,
+} from "lucide-react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -31,42 +38,64 @@ type HandleType = {
   breadcrumb: (param?: string) => React.ReactNode;
 };
 const admin_routes = [
-  { label: "Dashboard", href: "/admin/dashboard" },
-  { label: "Posts", href: "/admin/posts" },
-  { label: "Profile", href: "/admin/profile" },
-  { label: "Action Center", href: "/admin/actioncenter" },
+  {
+    label: "Dashboard",
+    href: "/admin/dashboard",
+    icon: <LayoutDashboard></LayoutDashboard>,
+  },
+  { label: "Posts", href: "/admin/posts", icon: <SquareGantt></SquareGantt> },
+  { label: "Profile", href: "/admin/profile", icon: <UserCog></UserCog> },
+  {
+    label: "Action Center",
+    href: "/admin/actioncenter",
+    icon: <LocateFixed></LocateFixed>,
+  },
 ];
 
 export default function Admin_Layout() {
   const [show, setShow] = useState<boolean>(false);
+  const [parent] = useAutoAnimate(/* optional config */);
+  useEffect(() => {}, [show]);
   return (
     <div className=" flex h-[100dvh] items-start justify-start bg-slate-300">
       <aside
-        className="  flex h-full w-[83px] flex-col items-start justify-start gap-4 bg-slate-500 px-5 pt-10 transition-all duration-200 ease-in-out hover:w-[280px]"
+        className="  flex h-full w-[83px] flex-col items-center justify-start gap-4 bg-zinc-200 px-2 pt-10 transition-all duration-200 ease-in-out hover:w-[280px] "
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
+        ref={parent}
       >
         {admin_routes.map((ar) => {
           return (
-            <NavLink
-              key={ar.label}
-              to={ar.href}
-              // className={"text-xl text-white"}
-            >
-              {({ isActive, isPending }) => (
-                <Button
-                  variant={"outline"}
-                  className={
-                    isPending
-                      ? "bg-blue-400"
-                      : isActive
-                        ? " border-none bg-slate-600  font-bold text-white hover:bg-green-600 hover:text-white"
-                        : "w-fit border-black bg-transparent text-white hover:border-none hover:bg-green-600 hover:font-semibold hover:text-white"
-                  }
-                >
-                  {show ? ar.label : "Icon"}
-                </Button>
-              )}
+            <NavLink key={ar.label} to={ar.href} className={""}>
+              {({ isActive, isPending }) =>
+                !show ? (
+                  <Button
+                    variant={"outline"}
+                    className={
+                      isPending
+                        ? "bg-blue-400"
+                        : isActive
+                          ? " border-none bg-slate-400  font-bold text-white   hover:bg-green-600 hover:text-white"
+                          : "w-fit border-black bg-transparent text-black/50  hover:border-none hover:bg-green-600 hover:font-semibold  hover:text-white"
+                    }
+                  >
+                    {ar.icon}
+                  </Button>
+                ) : (
+                  <Button
+                    variant={"outline"}
+                    className={
+                      isPending
+                        ? "bg-blue-400"
+                        : isActive
+                          ? " border-none bg-slate-400  font-bold text-white   hover:bg-green-600 hover:text-white"
+                          : "w-fit border-black bg-transparent text-black/50 hover:border-none hover:bg-green-600 hover:font-semibold hover:text-white"
+                    }
+                  >
+                    {ar.label}
+                  </Button>
+                )
+              }
             </NavLink>
           );
         })}
